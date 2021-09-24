9fbe5aa1a47a6c19bf1d9760f2ee0f7f98063a79
dawnduck07 <parkjongseo07@gmail.com>
2021-09-24 12:17:50 +0900

    Dog Cat class 추가

--- 01_hello-git/src/com/kh/animal/Cat/Cat.java
+++ 01_hello-git/src/com/kh/animal/Cat/Cat.java
@@ -2,7 +2,7 @@ package com.kh.animal.Cat;
 
 public class Cat {
 	
-	public static void main(String[] args) {
-		System.out.println("냐옹");
+	public void say() {
+		System.out.println("냐옹~");
 	}
 }
--- 01_hello-git/src/com/kh/animal/Dog/Dog.java
+++ 01_hello-git/src/com/kh/animal/Dog/Dog.java
@@ -2,7 +2,7 @@ package com.kh.animal.Dog;
 
 public class Dog {
 	
-	public static void main(String[] args) {
-		System.out.println("멍멍");
+	public void say() {
+		System.out.println("멍멍~");
 	}
 }
